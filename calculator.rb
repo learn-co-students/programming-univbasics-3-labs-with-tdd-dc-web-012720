aad7b6c9de62411e1ada5a1ecf106b7d106dbd90
Beza Sirak <sirak.bezawit@gmail.com>
2019-12-28 22:45:04 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,9 @@
 # Add your variables here
+
+first_number = 3 
+second_number = 5
+puts sum = first_number + second_number
+puts difference = first_number - second_number
+puts product = first_number * second_number
+puts quotient = first_number / second_number
+
